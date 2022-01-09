2cda6214767a2b253584ecc225817bb286eb099b
GitKeu <93443640+GitKeu@users.noreply.github.com>
2022-01-09 20:20:17 -0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,8 @@
-!bin/bash
+#!bin/bash
+GREEN='\033[0;32m'
 while :
 do
-echo " INICIANDO O KEUBOT
+echo "${GREEN} INICIANDO O KEUBOT
 ESTE ARQUIVO É PROGRAMADO PARA O BOT NÃO CAIR
 APÓS ALGUMA QUEDA DE CONEXÃO"
     npm start
